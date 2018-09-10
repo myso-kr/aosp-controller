1952df8d5aae6a0e1f789daee75a97542f8ba792
Choi Won <help@myso.kr>
2018-09-10 18:28:44 +0900

    no message

--- src/logger.js
+++ src/logger.js
@@ -6,6 +6,6 @@ module.exports = Logger.createLogger({
   format: Logger.format.combine(Logger.format.timestamp(), Logger.format.printf((info)=>`${info.timestamp}\t${info.level}\t${info.message}`)),
   transports: [
     new Logger.transports.Console(),
-    new Logger.transports.File({ filename: `console_${moment.format('YYYYMMDD')}.log` })
+    new Logger.transports.File({ filename: `console_${moment().format('YYYYMMDD')}.log` })
   ]
 })
